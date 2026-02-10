d77a2a57aa000611fcd272546fed517e2f13db20
Swabhiman Upadhyaya <swabhimanupadhyaya18@gmail.com>
2026-02-10 20:08:52 +0530

    use process.env.PORT for render

--- Class9-Full-Integration-of-Backend-and-Frontend/Backend/server.js
+++ Class9-Full-Integration-of-Backend-and-Frontend/Backend/server.js
@@ -5,6 +5,12 @@ const connectToDb = require("./src/config/database")
 
 connectToDb();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000")
-})
+// app.listen(3000, () => {
+//   console.log("Server is running on port 3000")
+// })
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
