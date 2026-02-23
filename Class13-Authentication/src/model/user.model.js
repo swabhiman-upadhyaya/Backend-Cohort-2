b687365df8d2faabe7d83c64ae1c12de8cc843e9
Swabhiman Upadhyaya <swabhimanupadhyaya18@gmail.com>
2026-02-23 23:53:59 +0530

    Ensure user can not create multiple account by using single mail and then creating and transfering token to the client side by cookie which is also accessible by the server

--- Class13-Authentication/src/model/user.model.js
+++ Class13-Authentication/src/model/user.model.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose")
 
 const userSchema = mongoose.Schema({
   name: String,
-  email: String,
+  email: {
+    type: String,
+    unique: [true, "User already exists"],
+  },
   password: String,
 })
 
--- Class13-Authentication/src/routes/auth.routes.js
+++ Class13-Authentication/src/routes/auth.routes.js
@@ -1,18 +1,37 @@
 const express = require("express");
 const userModel = require("../model/user.model");
+const jwt = require("jsonwebtoken");
 
 const authRouter = express.Router();
 
 authRouter.post("/register", async (req, res) => {
   const { name, email, password } = req.body
 
+  const isUserAlreadyExist = await userModel.findOne({ email })
+
+  if (isUserAlreadyExist) {
+    return res.status(400).json({
+      message: "User already exists from this email"
+    })
+  }
+
   const userDetails = await userModel.create({
     name, email, password
   })
 
+  const token = jwt.sign(
+    {
+      id: userDetails._id
+    },
+    process.env.JWT_SECRET
+  )
+
+  res.cookie("jwt_token", token);
+
   res.status(201).json({
     message: "Used Registered",
-    userDetails
+    userDetails,
+    token
   })
 })
 
