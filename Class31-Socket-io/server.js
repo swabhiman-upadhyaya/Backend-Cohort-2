20f1cec93afd58a5b8052552daccc93c7f752464
Swabhiman Upadhyaya <swabhimanupadhyaya18@gmail.com>
2026-03-25 23:49:30 +0530

    socket.io: io => server side socket => single user on => event listen emit => event fire

---
+++ Class31-Socket-io/server.js
@@ -0,0 +1,31 @@
+import app from "./src/app.js"
+
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {});
+
+io.on("connection", (socket) => {
+  console.log("New connection created")
+
+  socket.on("message", (msg) => {
+    console.log("Single user fired message event")
+    
+    console.log(msg)
+    io.emit("abc")
+    socket.emit("abc")
+  })
+
+});
+
+httpServer.listen(3000, () => {
+  console.log("Server is running on port 3000")
+});
+
+/* TASK TO READ ABOUT THESE THINGS */
+/**
+ * socket.emit()
+ * socket.broadcast().emit()
+ * io.emit()
+ */
